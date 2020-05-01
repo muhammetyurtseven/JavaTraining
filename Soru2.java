424b1a7181b73aded174f9c1e42c110303202664
muhammetyurtseven <64220554+muhammetyurtseven@users.noreply.github.com>
2020-05-01 16:52:54 +0300

    Delete Soru2.java

--- Soru2.java
+++
@@ -1,9 +0,0 @@
-public static void main(String... args) 
-{
-
-String chair, table = "metal"; //Ýkinci satýrda chair deðiþkenine bir þey atanmadýðý için derlenmez.
-chair = chair + table;
-
-System.out.println(chair);
-
-}
