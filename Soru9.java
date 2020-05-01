71ff8e1d44a307e2cbecfc8f0fb559cec09a9f81
muhammetyurtseven <64220554+muhammetyurtseven@users.noreply.github.com>
2020-05-01 16:53:45 +0300

    Delete Soru9.java

--- Soru9.java
+++
@@ -1,9 +0,0 @@
-public class Values {
-integer a = Integer.valueOf("1"); //String tipini int tipine dönüþtürmek
-public static void main(String[] nums) 
-    {
-integer a = Integer.valueOf("2");
-integer b = Integer.valueOf("3");
-System.out.println(a + b); // iki tane a deðiþkeni oldugundan derlenmiyor.
-    }
-}
