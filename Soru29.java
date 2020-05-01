a6ddafd9e9a5543e19a4a89e2bbe9b0294e61a6e
muhammetyurtseven <64220554+muhammetyurtseven@users.noreply.github.com>
2020-05-01 16:53:10 +0300

    Delete Soru29.java

--- Soru29.java
+++
@@ -1,7 +0,0 @@
-public class TennisBall 
-{ public TennisBall() 
-{ System.out.println("bounce"); } 
-public static void main(String[] slam) {
-
-new TennisBall(); //constructor oldugundan dolayý yeni bir anahtar çaðýrmak gerekir.
- }}
